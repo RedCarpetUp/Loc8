dd7f6e04f7d675123e6e7a7c8e747f0462afda0f
Balram Pandey <balrampandey19@gmail.com>
2017-07-26 13:35:07 +0530

    Mock  location utils

---
+++ locationdetector/src/main/java/com/redcarpetup/locationdetector/MockLocationUtils.java
@@ -0,0 +1,105 @@
+package com.redcarpetup.locationdetector;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by redcarpet on 7/26/17.
+ */
+
+public class MockLocationUtils {
+    private static Context mContext;
+
+    public MockLocationUtils(Context context) {
+        this.mContext = context;
+    }
+
+    public static boolean isMockSettingsON(Context context) {
+        return !Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals("0");
+    }
+
+    public static boolean areThereMockPermissionApps(Context context) {
+        int count = 0;
+        PackageManager pm = context.getPackageManager();
+        List<ApplicationInfo> packages =
+                pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo applicationInfo : packages) {
+            try {
+                PackageInfo packageInfo = pm.getPackageInfo(applicationInfo.packageName,
+                        PackageManager.GET_PERMISSIONS);
+                String[] requestedPermissions = packageInfo.requestedPermissions;
+
+                if (requestedPermissions != null) {
+                    for (int i = 0; i < requestedPermissions.length; i++) {
+                        if (requestedPermissions[i]
+                                .equals("android.permission.ACCESS_MOCK_LOCATION")
+                                && !applicationInfo.packageName.equals(context.getPackageName())) {
+                            count++;
+                        }
+                    }
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e("Got exception ", e.getMessage());
+            }
+        }
+
+        if (count > 0)
+            return true;
+        return false;
+    }
+
+    public static boolean isLocationFromMockProvider(Context context, Location location) {
+        boolean isMock = false;
+        if (location != null) {
+            try {
+                if (android.os.Build.VERSION.SDK_INT >= 18) {
+                    isMock = location.isFromMockProvider();
+                } else {
+                    if (Settings.Secure.getString(context.getContentResolver(),
+                            Settings.Secure.ALLOW_MOCK_LOCATION).equals("0"))
+                        return false;
+                    else {
+                        return true;
+                    }
+                }
+            } catch (Exception e) {
+            }
+        }
+
+        return isMock;
+    }
+    public static boolean isLocationEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        }else{
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+
+
+    }
+}
