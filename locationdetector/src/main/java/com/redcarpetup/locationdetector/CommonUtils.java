7f2e7682ff38c7542b30fbaf9787a4c7e4f42ddc
Balram Pandey <balrampandey19@gmail.com>
2017-07-28 17:11:58 +0530

    isLocationEnabled

--- locationdetector/src/main/java/com/redcarpetup/locationdetector/CommonUtils.java
+++ locationdetector/src/main/java/com/redcarpetup/locationdetector/CommonUtils.java
@@ -1,6 +1,9 @@
 package com.redcarpetup.locationdetector;
 
 import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.TextUtils;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -13,14 +16,35 @@ public class CommonUtils {
 
     // Checking is Google play service available
 
-    public static boolean idPlayServiceAvailable(Context context) {
-        boolean idPlayService;
+    public static boolean isPlayServiceAvailable(Context context) {
+        boolean isPlayService;
         int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(context);
         if (status == ConnectionResult.SUCCESS) {
-            idPlayService = true;
+            isPlayService = true;
         } else {
-            idPlayService = false;
+            isPlayService = false;
         }
-        return idPlayService;
+        return isPlayService;
+    }
+    public static boolean isLocationEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        }else{
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+
+
     }
 }
