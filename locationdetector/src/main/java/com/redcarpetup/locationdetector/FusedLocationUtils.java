9b6f4329925200d2f5af37dd5d971ea0bf86e8e5
Balram Pandey <balrampandey19@gmail.com>
2017-07-29 21:44:47 +0530

    FusedLocationUtils

--- locationdetector/src/main/java/com/redcarpetup/locationdetector/FusedLocationUtils.java
+++ locationdetector/src/main/java/com/redcarpetup/locationdetector/FusedLocationUtils.java
@@ -5,13 +5,11 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
-import com.google.android.gms.location.LocationListener;
-
 /**
  * Created by redcarpet on 7/27/17.
  */
 
-public class FusedLocationUtils extends Service implements GooglePlayServicesClient.ConnectionCallbacks,GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
+public class FusedLocationUtils extends Service imGooglePlayServicesClient.ConnectionCallbacks,GooglePlayServicesClient.OnConnectionFailedListener,LocationListener{
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
