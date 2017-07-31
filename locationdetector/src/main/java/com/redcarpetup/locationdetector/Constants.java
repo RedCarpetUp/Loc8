5a32bba6e618a987b4ce2d77b915899c3389972e
Balram Pandey <balrampandey19@gmail.com>
2017-07-31 16:06:01 +0530

    COnstants

---
+++ locationdetector/src/main/java/com/redcarpetup/locationdetector/Constants.java
@@ -0,0 +1,11 @@
+package com.redcarpetup.locationdetector;
+
+/**
+ * Created by redcarpet on 7/31/17.
+ */
+
+public class Constants {
+
+    public static String ProviderType = "providerType";
+
+}
